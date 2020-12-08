23e9fe207d3f43f1d6052c1ef139b101181be188
Venables, Bill (Data61, Dutton Park Qld) <Bill.Venables@gmail.com>
2020-12-09 09:55:40 +1000

    Initial commit

---
+++ R/requireData.R
@@ -0,0 +1,96 @@
+#' Attach Package and Data
+#'
+#' Attaches a package and its dependencies to the search path
+#' including any data sets if the package itself, or any of its
+#' dependencies, have data sets available, but lazyLoad set to false
+#'
+#' @param package The name of the package
+#' @param lib.loc as for base::require()
+#' @param quietly as for base::require()
+#' @param character.only as for base::require()
+#' @param warn.conflicts as for base::require()
+#' @param reallyQuietly logical: suppress startup messages?
+#' @param ... extra arguments, currently ignored.
+#'
+#' @return logical: as for base::require()
+#' @export
+#'
+#' @examples
+#' requireData(lazyData)
+requireData <- function(package = stop("you must specify a package"),
+                        lib.loc = NULL, quietly = TRUE, character.only = FALSE,
+                        warn.conflicts = TRUE, reallyQuietly = quietly, ...) {
+  if(!character.only) {
+    pkg <- substitute(package)
+    if(!is.character(pkg)) pkg <- deparse(pkg)
+  } else pkg <- as.character(package)
+
+  if(length(pkg) != 1)
+    stop("only one package may be attached in any call")
+
+  ## check if the package is on, or can be put on, the search path:
+  s0 <- search()
+  oldWarn <- options(warn = -1)
+  on.exit(options(oldWarn))
+  OK <- if(reallyQuietly) {
+    suppressMessages(require(package = pkg, lib.loc = lib.loc,
+                                           quietly = TRUE, warn.conflicts = FALSE,
+                                           character.only = TRUE))
+  } else {
+    require(package = pkg, lib.loc = lib.loc, quietly = quietly,
+            warn.conflicts = warn.conflicts, character.only = TRUE)
+  }
+  options(oldWarn)
+  if(!OK) {
+    warning("no valid package called ", sQuote(pkg), " found")
+    return(invisible(FALSE))  ### package not installed
+  }
+
+  ## take care of any extra packages as well
+  packages <- unique(c(pkg, sub("^package:", "",
+                            grep("^package:", setdiff(search(), s0), value = TRUE))))
+
+  for(package in packages)
+      ## check if it needs to have lazy data
+      if(file.exists(f <- system.file("data", package = package)) &&
+         file.info(f)$isdir &&
+         !file.exists(file.path(f, "Rdata.rds"))) {
+
+        ## check if the package has any data sets
+        d <- utils::data(package = package)$results
+        if(nrow(d) == 0) next  ## no data sets to expose
+
+        d <- d[, "Item"]  ## names of data sets in form 'objName (dataName)'
+        objName <- sub(" .*$", "", d)
+        datName <- sub("^.*\\(", "", sub("\\)$", "", d))
+
+        ## remove any prior attachments of the datasets:
+        searchDataName <- paste0("datasets:", package)
+        while(searchDataName %in% search()) detach(searchDataName,
+                                                   character.only = TRUE)
+
+        ## find out where the package now sits on the search path:
+        searchPkgName <- paste0("package:", package)
+        pkgIndex <- match(searchPkgName, search(), nomatch = 1)
+
+        ## find out where to put the datasets and check OK:
+        pos <- pkgIndex + 1
+
+        ## set up the datasets entry and populate it with 'promises':
+        env <- attach(NULL, pos = pos, name = searchDataName)
+        attr(env, "path") <- attr(as.environment(pkgIndex), "path")
+
+        for(i in seq_along(d))
+            eval(substitute(delayedAssign(OBJ, {
+              data(DAT, package = PKG,
+                   envir = as.environment(match(SEARCHDATANAME, search())))
+              get(OBJ)
+            }, eval.env = env, assign.env = env),
+                            list(OBJ = objName[i],
+                                 SEARCHDATANAME = searchDataName,
+                                 PKG = package,
+                                 DAT = datName[i])))
+      }
+  invisible(TRUE)
+}
+
---
+++ R/use_pack.R
@@ -0,0 +1,70 @@
+#' @importFrom pingr is_online
+#' @importFrom utils install.packages
+NULL
+
+
+#' Add packages to the search path
+#'
+#' Make multiple packages available on the search path,
+#' optionally installing any which are not yet installed,
+#' (provided they are available on the default repository).
+#'
+#' @param ... names of packages required as unquoted character strings
+#' @param list package names as a character string vector (or list)
+#' @param quietly logical: should the package startup messages be stifled?
+#' @param show_data logical: should non-lazy-loaded data sets be exposed?
+#'
+#' @return A character string vector of the package names, invisibly
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' modelling <- cq(lme4, splines, mgcv, randomForest, rpart)
+#' use_packages(tidyverse, readxl, list = modelling, quietly = TRUE)
+#' }
+use_packages <- function (..., list = NULL, quietly = TRUE, show_data = TRUE) {
+  dots <- match.call(expand.dots = FALSE)$...
+  pkgs <- c(lapply(dots, deparse), as.list(list))
+  if(is.null(getOption("repos"))) {
+    options(repos = c(CRAN = "https://cloud.r-project.org"))
+    on.exit(options(repos = NULL))
+  }
+  req <- if(show_data) requireData else if(quietly) {
+    function(...) {
+      suppressPackageStartupMessages(require(...))
+    }
+  } else base::require
+  for (pkg in pkgs) {
+    if (!req(pkg, character.only = TRUE, quietly = quietly)) {
+      if(is_online()) {
+        install.packages(pkg)
+      } else {
+        stop("You appear to be offline, and package ",
+             sQuote(pkg), " is not yet installed.")
+      }
+      req(pkg, character.only = TRUE, quietly = quietly)
+    }
+  }
+  # }
+  invisible(pkgs)
+}
+
+#' Unquoted String Concatenation
+#'
+#' A convenience function.  Allows a character string
+#' vector to be generated without typing the quotation
+#' marks.  Very similar to Hmisc::Cs
+#'
+#' @param ... Unqouted strings
+#'
+#' @return A character string vector
+#' @export
+#'
+#' @examples
+#' packages <- cq(tidyverse, readxl, lme4, mgcv, splines, randomForest, rpart)
+#' packages
+cq <- function (...) {  ## same idea as Hmisc::Cs
+  sapply(match.call(expand.dots = FALSE)$..., deparse)
+}
+
+
